3ba193185d186bdba6e63ae4a5db05e1cfca5fdb
Fabricio Andres Carrio Rodriguez <fabricio.carrio@jussanjuan.gov.ar>
2026-08-26 13:09:38 -0300

    feat: alternar dinámicamente entre Esri Light Gray y Esri Dark Gray según el tema claro u oscuro de la app

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -957,13 +957,15 @@ class _MapScreenState extends State<MapScreen>
               ),
               children: [
                 TileLayer(
-                  urlTemplate:
-                      'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+                  urlTemplate: isDark
+                      ? 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+                      : 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
                   userAgentPackageName: 'com.walkwin.app',
                 ),
                 TileLayer(
-                  urlTemplate:
-                      'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer/tile/{z}/{y}/{x}',
+                  urlTemplate: isDark
+                      ? 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Reference/MapServer/tile/{z}/{y}/{x}'
+                      : 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer/tile/{z}/{y}/{x}',
                   userAgentPackageName: 'com.walkwin.app',
                 ),
                 if (_exploredTiles.isNotEmpty)

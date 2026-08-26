d5f37512a91e2139c606ec84c8096c11a087b6b8
Fabricio Andres Carrio Rodriguez <fabricio.carrio@jussanjuan.gov.ar>
2026-08-26 11:10:12 -0300

    fix: cambiar a Esri World Light Gray Canvas basemap minimalista sin clave API ni marcas de agua

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -958,8 +958,12 @@ class _MapScreenState extends State<MapScreen>
               children: [
                 TileLayer(
                   urlTemplate:
-                      'https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png',
-                  subdomains: const ['a', 'b', 'c', 'd'],
+                      'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+                  userAgentPackageName: 'com.walkwin.app',
+                ),
+                TileLayer(
+                  urlTemplate:
+                      'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer/tile/{z}/{y}/{x}',
                   userAgentPackageName: 'com.walkwin.app',
                 ),
                 if (_exploredTiles.isNotEmpty)
